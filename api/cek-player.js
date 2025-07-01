07c5b7c6dfbfe0ef49229b48b7a5263b4d34df70
Mochamad Reza Rahadi <sencative@gmail.com>
2025-07-01 22:00:28 +0700

    Update cek-player.js

--- api/cek-player.js
+++ api/cek-player.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const response = await fetch(`${process.env.SERVERTAP_URL}/v1/players/all`, {
+    const response = await fetch(`http://45.59.168.36:4568/v1/players/all`, {
       headers: {
         "Authorization": `Bearer ` + 'andarakerenbangetturqouisejaya'
       }
